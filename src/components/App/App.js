4fb2f7b891527de287b8f81423676771ec270501
ForTheOnesThere <jamescockayne1@gmail.com>
2020-06-20 22:05:47 +0100

    Made launch countdown component with basic functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Countdown from '../Countdown/Countdown';
 
 function App() {
   return (
     <div className="App">
-      <h1>SpaceXpedia!</h1>
+      <Countdown />
     </div>
   );
 }
---
+++ src/components/Countdown/Countdown.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+class Countdown extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      nameOfNextLaunch: 'Loading...',
+      nextLaunchTimeUnix: 'Loading...',
+      tMinusTime: 'Loading...',
+    }
+  }
+
+  render(){
+    return(
+      <section id='countdown'>
+        <h1>Lift-off in {this.state.tMinusTime}</h1>
+      </section>
+    )
+  }
+
+  componentDidMount(){
+    // Say whether the component is mounted and request the data from API
+    console.log(`Countdown component mounted successfully!`);
+    const data = this.fetchLaunchData();
+
+    // When the data is recieved, update the state...
+    data.then(result => {
+    this.setState({nameOfNextLaunch: result.mission_name, nextLaunchTimeUnix: result.launch_date_unix});
+      }).catch(e=>console.log(e));
+
+    // ...and then continuusly work out the 't-minus' time to launch, updating the state everytime it changes
+    setInterval(() => this.updateTminusString(this.state.nextLaunchTimeUnix),100);
+
+
+
+  }
+
+  updateTminusString = (date) => {
+    // Updates the state to reflect the time to the input, the 'date' variable
+    let timeOfLaunch_unix = date;
+
+    let currentTimeUnixRaw = Date.now()/1000;
+    let currentTimeUnix = Math.floor(currentTimeUnixRaw);
+    //.log(`The UNIX time of launch is ${timeOfLaunch_unix}`);
+    //console.log(`The UNIX time now is ${currentTimeUnix}`);
+
+    let secondsToLaunch = timeOfLaunch_unix - currentTimeUnix;
+    //console.log(`There are ${secondsToLaunch} seconds until launch.`);
+
+    let rawDays = secondsToLaunch/86400;
+    let roundedDays = Math.floor(rawDays);
+    //console.log(`There are ${roundedDays} whole days until launch.`);
+
+    let rawHours = (secondsToLaunch%86400)/3600;
+    let roundedHours = Math.floor(rawHours);
+    //console.log(`There are ${roundedHours} whole hours until launch.`);
+
+    let rawMinutes = ((secondsToLaunch%86400)%3600)/60;
+    let roundedMinutes = Math.floor(rawMinutes);
+    //console.log(`There are ${roundedMinutes} whole minutes until launch.`);
+
+    let seconds = secondsToLaunch - (86400*roundedDays) - (3600*roundedHours) - (60*roundedMinutes);
+    //console.log(`Finally, there are ${seconds} seconds left until launch.`);
+
+    //let longFormTime = `There are ${roundedDays} days, ${roundedHours} hours, ${roundedMinutes} minutes and ${seconds} seconds until launch.`;
+    let tMinusTime = `T- ${('00' + roundedDays).substr(-3)}:${('0' + roundedHours).substr(-2)}:${('0' + roundedMinutes).substr(-2)}:${('0' + seconds).substr(-2)}`
+    this.setState({tMinusTime: tMinusTime});
+  }
+
+  fetchLaunchData = async () => {
+    const response = await fetch('https://api.spacexdata.com/v3/launches/next');
+    const data = await response.json();
+    return data;
+  }
+
+}
+
+export default Countdown;
