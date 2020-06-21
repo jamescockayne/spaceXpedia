e6af3b0b17977e5fa84cf4a0292c4160278126e7
ForTheOnesThere <jamescockayne1@gmail.com>
2020-06-21 07:14:23 +0100

    Updates

---
+++ precache-manifest.ff20374d20bf59442959315975519a3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae10997c119082d032980e86a463218b",
+    "url": "/spaceXpedia/index.html"
+  },
+  {
+    "revision": "97e7b3ed130e11f6bfad",
+    "url": "/spaceXpedia/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3999311e5af2b8a7e175",
+    "url": "/spaceXpedia/static/css/main.083a4c8b.chunk.css"
+  },
+  {
+    "revision": "97e7b3ed130e11f6bfad",
+    "url": "/spaceXpedia/static/js/2.47400c52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spaceXpedia/static/js/2.47400c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3999311e5af2b8a7e175",
+    "url": "/spaceXpedia/static/js/main.35f381a9.chunk.js"
+  },
+  {
+    "revision": "1d037b93dd419c1d5760",
+    "url": "/spaceXpedia/static/js/runtime-main.35a29c35.js"
+  },
+  {
+    "revision": "572190d9955d8479ce4c3964f891d33a",
+    "url": "/spaceXpedia/static/media/VCROSD.572190d9.woff2"
+  },
+  {
+    "revision": "ca9689d8791c0c76f4f6d8b310222ad6",
+    "url": "/spaceXpedia/static/media/VCROSD.ca9689d8.ttf"
+  },
+  {
+    "revision": "eef19ff8224f5785aa03cfd31b9ddd51",
+    "url": "/spaceXpedia/static/media/VCROSD.eef19ff8.woff"
+  },
+  {
+    "revision": "d5258c1440b93b938c4974ab79ddfe43",
+    "url": "/spaceXpedia/static/media/rocketBackground.d5258c14.jpg"
+  }
+]);
