a8c6073562b7dfc0a185ffeb98e0c6750e36766e
ForTheOnesThere <jamescockayne1@gmail.com>
2020-06-21 06:19:16 +0100

    Minimum viable product for Launch Information component complete

--- src/components/LaunchInformation/LaunchInformation.js
+++ src/components/LaunchInformation/LaunchInformation.js
@@ -5,7 +5,14 @@ class LaunchInformation extends Component {
   constructor(props){
     super(props)
     this.state = {
-
+      patchUrl:  '',
+      launchInformation: 'Loading...',
+      flightNumber: 'Loading...',
+      launchSite: 'Loading...',
+      landingZone: 'Loading...',
+      missionName: 'Loading...',
+      redditLink: 'Loading...',
+      boosterFlightNumber: 'Loading...',
     }
   }
 
@@ -18,15 +25,31 @@ class LaunchInformation extends Component {
 
             <div className='info-top'>
               <div>
-                <p>This is the top image.</p>
+                <img src={this.state.patchUrl} alt='Mission Patch' />
               </div>
-              <div>
-                <p>This is the top information.</p>
+              <div className='launch-facts'>
+                <p>Mission: {this.state.missionName}
+                  <br></br>
+                  <br></br>
+                  This is launch number {this.state.flightNumber} for SpaceX
+                  <br></br>
+                  <br></br>
+                  Launch Site: {this.state.launchSite}
+                  <br></br>
+                  <br></br>
+                  Landing Zone: {this.state.landingZone}
+                  <br></br>
+                  <br></br>
+                  The first stage core has flown {this.state.boosterFlightNumber-1} times before. This will be flight number {this.state.boosterFlightNumber}.
+                  <br></br>
+                  <br></br>
+                  Be a part of the launch campaign with us on <a href={this.state.redditLink}>Reddit</a>!
+                </p>
               </div>
             </div>
 
             <div className='info-bottom'>
-              <p>This is the bottom.</p>
+              <p className='scrollable'>{this.state.launchInformation}</p>
             </div>
 
           </div>
@@ -43,7 +66,20 @@ class LaunchInformation extends Component {
 
     // When the data is recieved, update the state...
     data.then(result => {
-    this.setState({});
+    let landingRaw = result.rocket.first_stage.cores[0].landing_vehicle;
+    let landingZone;
+    if (landingRaw === 'JRTI') {landingZone = 'Just Read The Instructions'}
+    else if (landingRaw === 'OCISLY') {landingZone = 'Of Course I Still Love You'}
+    else {landingZone = landingRaw};
+
+    this.setState({patchUrl: result.links.mission_patch,
+                   launchInformation: result.details,
+                   flightNumber: result.flight_number,
+                   launchSite: result.launch_site.site_name_long,
+                   landingZone: landingZone,
+                   missionName: result.mission_name,
+                   redditLink: result.links.reddit_campaign,
+                   boosterFlightNumber: result.rocket.first_stage.cores[0].flight});
       }).catch(e=>console.log(e));
 
     //page refresh will be needed to update after a launch
