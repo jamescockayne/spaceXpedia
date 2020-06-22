cb31bf70687edb78eb467473124044634ba9cc17
ForTheOnesThere <jamescockayne1@gmail.com>
2020-06-22 19:25:13 +0100

    Fleshed out the detail view

--- src/components/DetailWindow/DetailWindow.js
+++ src/components/DetailWindow/DetailWindow.js
@@ -2,15 +2,28 @@ import React from 'react';
 
 import '../../styles/DetailWindow.css';
 
-const DetailWindow = () => {
+const DetailWindow = (props) => {
+
+  let database = props.database;
+  let requestedResource = database.resourceRequested;
+  let date = new Date(database.datesUNIX[requestedResource]*1000);
+  console.log(database);
 
   return (
+
   <section id='detail-screen'>
     <div className='detail-window-container'>
-      This is the detail view.
+
+      <img src={database.patchUrls[requestedResource]} alt='Mission Patch' /><br/>
+      Mission: {database.names[requestedResource]} <br/>
+      Launch Site: {database.launch_sites[requestedResource]}<br/>
+      Date: {date.toGMTString()}<br/>
+      Details: {database.details[requestedResource]} <br/>
+      Link: <a href={database.videoUrls[requestedResource]} target="_blank" rel="noopener noreferrer">Video link</a> <br/>
+      Orbit: {database.orbits[requestedResource]}<br/>
+
     </div>
   </section>
-
 
   )
 
