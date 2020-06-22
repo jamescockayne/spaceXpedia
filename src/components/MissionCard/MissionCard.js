7e5986800b095f56725e7583b0b28947cc6bbf73
ForTheOnesThere <jamescockayne1@gmail.com>
2020-06-22 20:57:35 +0100

    Styling updates

--- src/components/MissionCard/MissionCard.js
+++ src/components/MissionCard/MissionCard.js
@@ -5,11 +5,9 @@ import '../../styles/Card.css';
 const MissionCard = ({ photoLink, missionName, id, resourceClick}) => {
   return (
     <div key={id} id={id} onClick={() => resourceClick(id)} className = 'mission-card grow tc dib br4 pa3 ma2 bw2 shadow-5'>
-      <div>
-        <img className='card-image' src={photoLink} alt='missionName' />
-      </div>
       <div className='card-title-container'>
-        {missionName}
+      <img className='card-image' src={photoLink} alt='missionName' />
+        <p className='mission-title'>{missionName}</p>
       </div>
     </div>
   )
